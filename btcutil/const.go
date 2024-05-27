2ef5b08935e658381ac0904c68e0f6f93517d8da
howard work <haohxu@yahoo.com>
2024-05-27 18:00:15 +0800

    Signed-off-by: howard work <haohxu@yahoo.com>

---
+++ btcutil/const.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2013-2014 The btcsuite developers
+// Copyright (c) 2018-2021 The Omegasuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package btcutil
+
+const (
+	// HaoPerBitcent is the number of hao in one bitcoin cent.
+	HaoPerBitcent = 1e6
+
+	// HaoPerBitcoin is the number of hao in one bitcoin (1 OMC).
+	HaoPerBitcoin = 1e8
+
+	// MaxHao is the maximum transaction amount allowed in hao.
+	MaxHao = 430e6 * HaoPerBitcoin
+)
---
+++ btcutil/sig.go
@@ -0,0 +1,57 @@
+// Copyright (c) 2013-2016 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package btcutil
+
+import (
+	"fmt"
+	"github.com/omegasuite/btcd/btcec"
+	"github.com/omegasuite/famofchains/btcd/chaincfg"
+	"github.com/omegasuite/famofchains/omega"
+)
+
+func VerifySigScript(sign, hash []byte, chainParams *chaincfg.Params) (*AddressPubKeyHash, error) {
+	if chainParams == nil {
+		return nil, nil
+	}
+	if len(sign) < btcec.PubKeyBytesLenCompressed {
+		return nil, fmt.Errorf("Incorrect signature")
+	}
+	k, err := btcec.ParsePubKey(sign[:btcec.PubKeyBytesLenCompressed], btcec.S256())
+	if err != nil {
+		return nil, fmt.Errorf("Incorrect Miner signature. pubkey error")
+	}
+
+	pk, _ := NewAddressPubKeyPubKey(*k, chainParams)
+	pk.pubKeyFormat = PKFCompressed
+	s, err := btcec.ParseSignature(sign[btcec.PubKeyBytesLenCompressed:], btcec.S256())
+	if err != nil {
+		return nil, fmt.Errorf("Incorrect Miner signature. Signature parse error")
+	}
+
+	if !s.Verify(hash, pk.PubKey()) {
+		return nil, fmt.Errorf("Incorrect Miner signature. Verification doesn't match")
+	}
+
+	return pk.AddressPubKeyHash(), nil
+}
+
+func VerifySigScript2(sign, hash, kubkey []byte, chainParams *chaincfg.Params) omega.Err {
+	k, err := NewAddressPubKey(kubkey, chainParams)
+
+	if err != nil {
+		return omega.ScriptError(omega.ErrInternal, "Incorrect Miner signature. pubkey error")
+	}
+
+	s, err := btcec.ParseSignature(sign, btcec.S256())
+	if err != nil {
+		return omega.ScriptError(omega.ErrInternal, "Incorrect signature. Signature parse error")
+	}
+
+	if !s.Verify(hash, k.PubKey()) {
+		return omega.ScriptError(omega.ErrInternal, "Incorrect signature. Verification doesn't match")
+	}
+
+	return nil
+}
